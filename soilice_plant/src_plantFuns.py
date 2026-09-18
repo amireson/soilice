751ce91c4123012fd158cdcee669b63e77890ec6
Andrew Ireson <andrew.ireson@usask.ca>
2026-09-17 21:06:53 -0600

    penman monteith

--- soilice_plant/src_plantFuns.py
+++ soilice_plant/src_plantFuns.py
@@ -93,3 +93,70 @@ def getEvapFluxes(E_PT,E_PS,psie,T,z,dz,nt,pars,const):
     jE[0]=0.
 
     return E_AT,E_AS,jE
+
+@njit(inline='always')
+def gamma(p):  # gamma (KPa/deg c)
+    g = (0.665/1000)*p # p Atmo. pressure (KPa)
+    return g
+
+@njit(inline='always')
+def Delta(T,es):  # Delta (KPa/deg c)
+    d = (4098*es)/(T+237.3)**2
+    return d
+
+@njit(inline='always')
+def satvappres(T): # saturation vapour pressure (KPa)
+    es = 0.6108*np.exp((17.27*T)/(T+237.3))
+    return es
+
+@njit(inline='always')
+def vappres(RH,es): # actual vapour pressure (KPa)
+    ea = (RH/100)*es
+    return ea
+
+@njit(inline='always')
+def wind(uz,z): # wind speed at 2m height (m/s)
+    u2 = uz*4.87/np.log(67.8*z-5.42) # uz wind speed m/s measured at hieght z (m)
+    return u2
+
+
+@njit(inline='always')
+def GetPotentialEvap(SWnet,LWnet,T,p,RH,uz,z,zavg,rs): # input data: SW&LW(W/m2), T(deg c), p(Pa), RH(%), uz(m/s), z(m)
+    
+    #Equation parameters with the righ units:    
+    
+    Rn=(SWnet+LWnet)*(0.0864)   # MJ/m2/d (as 30*60 is the number of secondes in period)
+    p = p/1000         # KPa
+    es=satvappres(T)   # KPa
+    ea=vappres(RH,es)  # KPa
+    g =gamma(p)        # KPa/deg c
+    delta= Delta(T,es)    # KPa/deg c
+    
+    # u2 (m/s)
+    if z==2:
+        u2=uz
+    else:
+        u2=wind(uz,z)
+        
+    # calculations for the aerodynamic resistance:
+    d  = (2/3)*zavg   #zero displacment height
+    z0 = 0.123*zavg # roughness length of momentum
+    k  = 0.41       # von Karman constant
+    
+    #1
+    ra = (np.log((2-d)/z0)*np.log((2-d)/(z0*0.1)))/(k**2)/u2  # the FAO eqaution (s/m)
+
+    #2
+#     Uf = (k*u2)/np.log((2-d)/z0) # Dingman book equation
+#     ra=u2/(Uf**2)
+    
+
+    # calculate the equation terms:
+    a = 0.408*delta*Rn
+    b = (g*185396.2*(es-ea)/((T+273)))*(1/ra)
+    c = delta+g*(1+(rs/ra))     #*(1+0.34*u2)
+    
+    PE = (a+b)/c
+    
+    PE[PE<0]=0
+    return PE
